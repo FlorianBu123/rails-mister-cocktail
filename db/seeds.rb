c3f4921c8a40246b38cfecc27e078ec98829386b
Florian Bucher <bucher.fl1@gmail.com>
2020-08-21 12:35:58 +0200

    Cloudify

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,8 @@
+require "open-uri"
+
 Ingredient.destroy_all
 Cocktail.destroy_all
 Dose.destroy_all
-
-Cocktail.create(name: "Old Fashioned")
-Cocktail.create(name: "Tequila Sunrise")
-Cocktail.create(name: "Bloody Mary")
 
 Ingredient.create( name: "Mint")
 Ingredient.create( name: "Orange Zest")
@@ -16,3 +14,16 @@ Ingredient.create( name: "Cream")
 Ingredient.create( name: "Basil")
 Ingredient.create( name: "Gin")
 Ingredient.create( name: "Absinthe")
+
+
+file = URI.open('https://cdn.liquor.com/wp-content/uploads/2015/08/woodford_oldfashd_2_800x800.png')
+cocktail = Cocktail.create(name: "Old Fashioned")
+cocktail.photo.attach(io: file, filename: 'old-fashioned.png', content_type: 'image/png')
+
+file = URI.open('https://giantbomb1.cbsistatic.com/uploads/original/9/99864/2419866-nes_console_set.png')
+cocktail2 = Cocktail.create(name: "Tequila Sunrise")
+cocktail2.photo.attach(io: file, filename: 'nes.png', content_type: 'image/png')
+
+file = URI.open('https://giantbomb1.cbsistatic.com/uploads/original/9/99864/2419866-nes_console_set.png')
+cocktail3 = Cocktail.create(name: "Bloody Mary")
+cocktail3.photo.attach(io: file, filename: 'nes.png', content_type: 'image/png')
